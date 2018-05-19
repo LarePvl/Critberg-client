6d927939324ef4f6d0351d3a11cca93569995f02
LarePVL <laurivir@gmail.com>
2018-05-19 19:58:15 +0300

    Added debug fixes and UI layout changes for testapp

--- test.js
+++ test.js
@@ -11,27 +11,35 @@ function toBuffer(json) {
   return new Buffer.from(JSON.stringify(json));
 }
 
+const PORT = process.env.PORT || '6969';
+const ADDRESS = process.env.ADDRESS || 'localhost';
+
 io.on('connection', websocket => {
-  console.log('a user connected');
-
   const socket = new net.Socket();
-  socket.connect('6969','localhost', () => {});
+  socket.connect(PORT, ADDRESS, () => {
+    websocket.on('data', data => {
+      console.log(data);
+      socket.write(toBuffer(data));
+    });
 
-  socket.on('data', data => {
-    const json = JSON.parse(data.toString());
-    websocket.emit('data', json);
-  })
-
-  websocket.on('data', data => {
-    console.log(data);
-    socket.write(toBuffer(data));
+    websocket.on('disconnect', () => {
+      socket.end(toBuffer({type:'disconnect'}));
+    });
   });
 
-  websocket.on('disconnect', () => {
-    socket.end(toBuffer({type:'disconnect'}));
+  socket.on('data', data => {
+    console.log(data.toString());
+    for (json of data.toString().split('\n').filter(v=>v!=='').map(JSON.parse)) {
+      websocket.emit('data', json);
+    }
+  });
+
+  socket.on('error', error => {
+    console.log(error.code);
   })
 });
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
+  console.log(`Server in ${ADDRESS}:${PORT}`);
 });
